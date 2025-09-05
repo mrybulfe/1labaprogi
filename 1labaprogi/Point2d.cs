a9cb0a2d9bc90e932d0c33a6f312313662a04e38
Vladislav <vladotegov253@gmail.com>
2025-09-05 10:21:29 +0700

    Добавлен класс Point2d, Canvas на форму

---
+++ 1labaprogi/Point2d.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1labaprogi
+{
+    public class Point2d
+    {
+        private int X;
+        private int Y;
+
+        public void Ponint2d(int x, int y)
+        {
+            this.X = x;
+            this.Y = y; 
+        }
+        public int getX()
+        {
+            return X;
+        }
+        public int getY()
+        {
+            return Y;
+        }
+
+        public void addX(int x)
+        {
+            X += x;
+        }
+        public void addY(int y)
+        {
+            Y += y;
+        }
+    }
+}
